bae82c37841fa61381132b64e5ae4ce27fa9fb61
EJG <gavis.edwin@gmail.com>
2020-05-09 17:19:54 -0500

    initial2

---
+++ make_model.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 19 14:44:26 2020
+
+@author: ejg
+"""
+
+import markovify
+import os
+
+corpus = ""
+files = os.listdir("corpus")
+for fname in files:
+    with open("corpus/" + fname) as f:
+        corpus += f.read()#.replace("\n"," ")
+
+text_model = markovify.NewlineText(corpus)
+with open("model.json", "w") as f:
+    f.write(text_model.to_json())
